6ad98aa86fcf4ec0111d45d66b1222f49c869dc0
alothmanmj <alothman.maj@gmail.com>
2023-09-18 15:09:50 +0300

    Created generating random groups method

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("bye, World!");
-        System.out.println(readFile("Student List.txt"));
+        System.out.println(generateGroups(readFile("Student List.txt"),4));
 
     }
     public static ArrayList<String> readFile(String filename){
@@ -22,5 +23,18 @@ public class App {
         }
         return arr;
         
+    }
+    public static ArrayList<ArrayList<String>> generateGroups(ArrayList<String> studentList, int  numOfGroups){
+        ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
+
+        Collections.shuffle(studentList);
+        for(int i = 0 ; i< numOfGroups; i++){
+            groups.add(new ArrayList<String>());
+        }
+
+        for(int i = 0  ; i < studentList.size(); i++ ){
+            groups.get(i%numOfGroups).add(studentList.get(i));
+        }
+        return groups;
     }
 }
