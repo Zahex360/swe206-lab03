b133458b66dc7b140113b35f82ab728d67c4a130
alothmanmj <alothman.maj@gmail.com>
2023-09-18 14:55:03 +0300

    Add a method to read the file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println(readFile("Student List.txt"));
+
+    }
+    public static ArrayList<String> readFile(String filename){
+        ArrayList arr = new ArrayList<>();
+        try {
+            File f = new File(filename);
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()){
+                arr.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e.getMessage());
+        }
+        return arr;
+        
     }
 }
