9212ec0f1dd6ffaf8b9838f8ed1d708c6d6dd19c
alothmanmj <alothman.maj@gmail.com>
2023-09-18 15:34:17 +0300

    back to hello world

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("bye, World!");
+        System.out.println("Hello, World!");
         System.out.println(generateGroups(readFile("Student List.txt"),4));
 
     }
