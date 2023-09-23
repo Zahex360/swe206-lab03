b1daeb75f65bd75ab57cf0d465b1267eadca2f9d
zahex360 <zahhod.sami@gmail.com>
2023-09-23 14:15:34 +0300

    step 7

---
+++ Sorter.java
@@ -0,0 +1,22 @@
+public class Sorter {
+    public static void selectionSort(String[] array) {
+
+        // Find the string reference that should go in each cell of
+        // the array, from cell 0 to the end
+        for (int j = 0; j < array.length - 1; j++) {
+            // Find min: the index of the string reference that should go into cell j.
+            // Look through the unsorted strings (those at j or higher) for the one that is
+            // first in lexicographic order
+            int min = j;
+            for (int k = j + 1; k < array.length; k++)
+                if (array[k].compareTo(array[min]) < 0)
+                    min = k;
+
+            // Swap the reference at j with the reference at min
+            String temp = array[j];
+            array[j] = array[min];
+            array[min] = temp;
+        }
+
+    }
+}
