a39ca44b68ec20dce8c5426df04f3a5395d07216
Eng.Mohammed <eng.mohammed.work@gmail.com>
2023-09-24 13:07:16 +0300

    Update for step 8

--- Sorter.java
+++ Sorter.java
@@ -20,18 +20,21 @@ public class Sorter {
 
     }
     
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;//searching for lowest index
-                }
+    public static void insertionSort(String[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            String key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            
+            arr[j + 1] = key;
         }
-    }
+}
 }
